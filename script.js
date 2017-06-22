4037128493289642e713f1b586e0e026a95ae711
Tiff Willson <tiff@roomhints.com>
2017-06-21 17:27:05 -0700

    files

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ $(document).ready(function () {
       var credential = error.credential;
       // ...
       });
+  });
 
     $(".logout").click(function() {
       console.log("logging out", firebase);
